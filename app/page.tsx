00ec0efb7df876614654c455cb54dffbf81a235a
ismaelbarcelos <186837887+ismaelbarcelos@users.noreply.github.com>
2026-05-08 20:18:28 -0300

    1.0

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Menu from "./components/menu";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>home</h1>
+      <Menu/>
+    </div>
+  );
+}
